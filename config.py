bbbd4a9216080be077e9738d26e3f4ecabae9154
harry <harrisongitau3@gmail.com>
2023-11-11 17:59:45 +0300

    Dev: Handle errors

--- config.py
+++ config.py
@@ -8,5 +8,5 @@ class Config(object):
 
     # Database
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.dib')
+        'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False

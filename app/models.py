7acf9aeb0ec58ace2c01680957c334d2bfbac156
12000KILLERRCODE <odhiambomikeok@gmail.com>
2023-11-16 15:40:38 +0300

    add new tables

--- app/models.py
+++ app/models.py
@@ -15,6 +15,9 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.String(128))
     performances = db.relationship('Performance', backref='author', lazy='dynamic')
     targets = db.relationship('Target', backref='author', lazy='dynamic')
+    players = db.relationship('Player', backref='author', lazy='dynamic')
+    coaching = db.relationship('Coaching', backref='author', lazy='dynamic')
+
 
 
     def __repr__ (self):
@@ -42,3 +45,20 @@ class Target(db.Model):
     year = db.Column(db.String(120), index=True)
     body = db.Column(db.Text)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+class Player(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(50), index=True)
+    lastname = db.Column(db.String(50), index=True)
+    date_of_birth = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    attendance = db.Column(db. String(200), index=True)
+    fitness = db.Column(db. Integer, index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+class Coaching(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    tactics = db.Column(db.Text)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    
+
+

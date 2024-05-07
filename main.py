f609ef3c4a60ee654050f8eac65675f0a6299276
12000KILLERRCODE <odhiambomikeok@gmail.com>
2024-05-07 16:16:14 +0300

    add models to main.py

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from app import app, db
-from app.models import User
+from app.models import User, Performance, Target, Player, Post, Coaching
 
 
 #add database instance and models to a flask sehell session
 @app.shell_context_processor
 def make_shell_context():
-    return dict(db=db, User=User)
+    return dict(db=db, User=User, Performance=Performance, Target=Target, Player=Player, Post=Post, Coaching=Coaching  )
+
+
+
+
+
+

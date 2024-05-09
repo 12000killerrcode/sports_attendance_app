086d8cf2a416cdef181f5b69f84070787caa9471
12000KILLERRCODE <odhiambomikeok@gmail.com>
2024-05-09 17:41:04 +0300

    modify main

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from app.models import User, Performance, Target, Player, Post, Coaching, EditPl
 #add database instance and models to a flask sehell session
 @app.shell_context_processor
 def make_shell_context():
-    return dict(db=db, User=User, Performance=Performance, Target=Target, Player=Player, Post=Post, Coaching=Coaching, Editplayer=Editplayer   )
+    return dict(db=db, User=User, Performance=Performance, Target=Target, Player=Player, Post=Post, Coaching=Coaching, EditPlayer=EditPlayer   )
 
 
 
